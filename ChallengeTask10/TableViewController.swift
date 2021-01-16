f09d44e16d91bbae41dce10720a52504f51493df
akio0911 <akio0911@gmail.com>
2021-01-16 15:42:58 +0900

    Feedback

--- ChallengeTask10/TableViewController.swift
+++ ChallengeTask10/TableViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
-    let prefecture = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",
+final class TableViewController: UITableViewController {
+    private let prefectures = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",
                "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県",
                "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県",
                "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県",
@@ -19,17 +19,16 @@ class TableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return prefecture.count
+        prefectures.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellId = ["Cell1", "Cell2", "Cell3"]
-        let identifier = cellId[indexPath.row % 3]
+        let cellIdentifiers = ["Cell1", "Cell2", "Cell3"]
+        let identifier = cellIdentifiers[indexPath.row % 3]
 
         let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
         // Configure the cell...
-        cell.textLabel?.text = prefecture[indexPath.row]
+        cell.textLabel?.text = prefectures[indexPath.row]
         cell.detailTextLabel?.text = "\(indexPath.row + 1)番目の都道府県です"
         cell.detailTextLabel?.textColor = .gray
 
